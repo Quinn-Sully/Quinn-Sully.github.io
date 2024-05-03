c0dcd7e70973c8d72811d0d4d209a8674de8cb56
Quinn-Sully <144717070+Quinn-Sully@users.noreply.github.com>
2024-05-03 18:49:46 +0000

    another one

--- scripts/main.js
+++ scripts/main.js
@@ -106,10 +106,10 @@ console.log(emparr10);
 console.log("May 5 tasks loaded");
 
 let colors = ["black", "orange", "tan", "silver", "maroon", "lime", "olive"];
-let unList = document.createElement(ul);
+let unList = document.createElement("ul");
 let listItem = 0
 for (i = 0; i<colors.length; i++){
-    listItem = document.createElement(li);
+    listItem = document.createElement("li");
     listItem.innerText(colors[i]);
     listItem.style.color = colors[i];
     unList.appendChild(listItem);
