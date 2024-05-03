8c412ff996df7bfa6aea56b2fe26b806b3722bd1
Quinn-Sully <144717070+Quinn-Sully@users.noreply.github.com>
2024-05-03 18:50:55 +0000

    another another one

--- scripts/main.js
+++ scripts/main.js
@@ -110,7 +110,7 @@ let unList = document.createElement("ul");
 let listItem = 0
 for (i = 0; i<colors.length; i++){
     listItem = document.createElement("li");
-    listItem.innerText(colors[i]);
+    listItem.innerText = colors[i];
     listItem.style.color = colors[i];
     unList.appendChild(listItem);
 }
